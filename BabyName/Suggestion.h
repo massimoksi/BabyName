fdb2e5d27eab803b8f1434538f59860c73b3aa08
Massimo Peri <massimoperi@gmail.com>
2014-09-09 21:57:14 +0200

    "Created model class"

---
+++ BabyName/Suggestion.h
@@ -0,0 +1,21 @@
+//
+//  Suggestion.h
+//  BabyName
+//
+//  Created by Massimo Peri on 09/09/14.
+//  Copyright (c) 2014 Massimo Peri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Suggestion : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic) int16_t gender;
+@property (nonatomic) int32_t language;
+@property (nonatomic) int16_t state;
+@property (nonatomic, retain) NSString * variants;
+
+@end
