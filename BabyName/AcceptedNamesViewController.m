f48ced5c9aa9bfaf99b32776f6ab71e4bb7bfba8
Massimo Peri <massimoperi@gmail.com>
2014-09-29 22:07:02 +0200

    Created fetched results controller to handle accepted names

--- BabyName/AcceptedNamesViewController.m
+++ BabyName/AcceptedNamesViewController.m
@@ -12,9 +12,11 @@
 #import "Suggestion.h"
 
 
-@interface AcceptedNamesViewController () <UITableViewDataSource>
+@interface AcceptedNamesViewController () <UITableViewDataSource, NSFetchedResultsControllerDelegate>
 
-@property (nonatomic, strong) NSMutableArray *acceptedNames;
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
+
+@property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
 
 @end
 
@@ -30,28 +32,32 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    
-    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
-    
-    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Suggestion"
-                                              inManagedObjectContext:self.managedObjectContext];
-    fetchRequest.entity = entity;
-    
+
     // Get new preferences from user defaults.
     NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
     NSInteger genders = [userDefaults integerForKey:kSettingsSelectedGendersKey];
     NSInteger languages = [userDefaults integerForKey:kSettingsSelectedLanguagesKey];
+
+#if DEBUG
+    NSLog(@"[AcceptedNamesViewController] User settings:");
+    NSLog(@"    Gender: %zd", genders);
+    NSLog(@"    Languages: %zd", languages);
+#endif
     
-    // Fetch all suggestions with state "maybe" and  matching the criteria from preferences.
+    // Fetch all suggestions with state "accepted" and  matching the criteria from preferences.
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"(state == %d) AND ((gender & %d) != 0) AND ((language & %d) != 0)", kSelectionStateAccepted, genders, languages];
-    fetchRequest.predicate = predicate;
+    self.fetchedResultsController.fetchRequest.predicate = predicate;
     
     NSError *error;
-    self.acceptedNames = [NSMutableArray arrayWithArray:[self.managedObjectContext executeFetchRequest:fetchRequest
-                                                                                                 error:&error]];
-    if (!self.acceptedNames) {
-        // TODO: handle the error.
+    if (![self.fetchedResultsController performFetch:&error]) {
+#if DEBUG
+        NSLog(@"[AcceptedNamesViewController] Error:");
+        NSLog(@"    Fetching error %@, %@", error, [error userInfo]);
+#endif
+        // TODO: handle error.
     }
+    
+    [self.tableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning
@@ -60,23 +66,57 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Accessors
+
+- (NSFetchedResultsController *)fetchedResultsController
+{
+    if (_fetchedResultsController) {
+        return _fetchedResultsController;
+    }
+
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    [fetchRequest setFetchBatchSize:20];
+
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Suggestion"
+                                              inManagedObjectContext:self.managedObjectContext];
+    fetchRequest.entity = entity;
+
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name"
+                                                                   ascending:YES];
+    [fetchRequest setSortDescriptors:@[sortDescriptor]];
+
+    _fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
+                                                                    managedObjectContext:self.managedObjectContext
+                                                                      sectionNameKeyPath:nil
+                                                                               cacheName:nil];
+    _fetchedResultsController.delegate = self;
+
+    return _fetchedResultsController;
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    return self.fetchedResultsController.sections.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.acceptedNames.count;
+    if (self.fetchedResultsController.sections.count > 0) {
+        id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchedResultsController.sections objectAtIndex:section];
+        return [sectionInfo numberOfObjects];
+    }
+    else {
+        return 0;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"AcceptedNameCell"];
     
-    Suggestion *suggestion = [self.acceptedNames objectAtIndex:indexPath.row];
+    Suggestion *suggestion = [self.fetchedResultsController objectAtIndexPath:indexPath];
     cell.textLabel.text = suggestion.name;
     
     return cell;
