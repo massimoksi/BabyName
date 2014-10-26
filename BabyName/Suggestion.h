c2943745fc7517409758cd70630685b6b29a5eda
Massimo Peri <massimoperi@gmail.com>
2014-10-26 22:49:27 +0100

    Recreated the model class

--- BabyName/Suggestion.h
+++ BabyName/Suggestion.h
@@ -2,15 +2,21 @@
 //  Suggestion.h
 //  BabyName
 //
-//  Created by Massimo Peri on 09/09/14.
+//  Created by Massimo Peri on 26/10/14.
 //  Copyright (c) 2014 Massimo Peri. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
 
-#import "SuggestionModel.h"
 
+@interface Suggestion : NSManagedObject
 
-@interface Suggestion : SuggestionModel
+@property (nonatomic) int16_t gender;
+@property (nonatomic) int32_t language;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic) int16_t state;
+
+@property (nonatomic, readonly) NSString *initials;
 
 @end
