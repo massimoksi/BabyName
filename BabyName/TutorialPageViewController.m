9611a2c7b2932ebd39c3b34f25913ee2bf12a419
Massimo Peri <massimoperi@gmail.com>
2014-11-27 23:59:28 +0100

    Created first 3 pages for tutorial

--- BabyName/TutorialPageViewController.m
+++ BabyName/TutorialPageViewController.m
@@ -38,7 +38,7 @@
     
     self.dataSource = self;
     
-    self.pageIdentifiers = @[@"Page1VC", @"Page2VC"];
+    self.pageIdentifiers = @[@"Page1VC", @"Page2VC", @"Page3VC"];
     
     PageViewController *pageViewController = [self.storyboard instantiateViewControllerWithIdentifier:self.pageIdentifiers.firstObject];
     [self setViewControllers:@[pageViewController]
