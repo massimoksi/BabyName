f65e18a70b91475eae703ab09c6d57ab148df226
Massimo Peri <massimoperi@gmail.com>
2015-01-02 23:33:15 +0100

    Minor changes

--- BabyName/FinishedViewController.m
+++ BabyName/FinishedViewController.m
@@ -50,18 +50,18 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - Navigation
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-    
-    if ([segue.identifier isEqualToString:@"ShowSettingsSegue"]) {
-        UINavigationController *settingsNavController = [segue destinationViewController];
-        settingsNavController.navigationBar.barStyle = UIStatusBarStyleLightContent;
-    }
-}
+//#pragma mark - Navigation
+//
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+//{
+//    // Get the new view controller using [segue destinationViewController].
+//    // Pass the selected object to the new view controller.
+//    
+//    if ([segue.identifier isEqualToString:@"ShowSettingsSegue"]) {
+//        UINavigationController *settingsNavController = [segue destinationViewController];
+//        settingsNavController.navigationBar.barStyle = UIStatusBarStyleLightContent;
+//    }
+//}
 
 #pragma mark - Actions
 
